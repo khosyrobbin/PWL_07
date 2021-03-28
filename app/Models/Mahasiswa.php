271abd1319d9edde6b76da407004902291dd99eb
khosy robbin <khosybin@gmail.com>
2021-03-28 11:32:45 +0700

    Add code at model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
-}
+    protected $table="mahasiswa";
+    public $timestamps= false;
+    protected $primaryKey = 'nim';
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'jurusan',
+        'no_handphone',
+    ];
+};
