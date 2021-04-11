006baf4bb10c5a506176c2b8184a6df594254297
khosy robbin <khosybin@gmail.com>
2021-04-11 19:40:26 +0700

    add code at seeder KelasSeeder

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KelasSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class KelasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $kelas = [
+            ['nama_kelas' => 'TI2A'],
+            ['nama_kelas' => 'TI2B'],
+            ['nama_kelas' => 'TI2C'],
+            ['nama_kelas' => 'TI2D'],
+            ['nama_kelas' => 'TI2E'],
+            ['nama_kelas' => 'TI2F'],
+            ['nama_kelas' => 'TI2G'],
+            ['nama_kelas' => 'TI2H'],
+            ['nama_kelas' => 'TI2I']
+        ];
+        DB::table('kelas')->insert($kelas);
     }
 }
